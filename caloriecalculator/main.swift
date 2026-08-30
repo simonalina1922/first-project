78cf57ae9edc1bffc52105cb83396d5a5f10e74c
Alina <chubchik1922@gmail.com>
2026-08-30 19:37:18 +0300

    Created calorie calculator

--- caloriecalculator/main.swift
+++ caloriecalculator/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+print("КАЛЬКУЛЯТОР КАЛОРИЙ")
+print("-------------------")
+
+print("Введите количество калорий в первом продукте:")
+guard let input1 = readLine(), let calories1 = Double(input1) else {
+    print("Ошибка: введено не число!")
+    exit(1)
+}
+
+print("Введите количество калорий во втором продукте:")
+guard let input2 = readLine(), let calories2 = Double(input2) else {
+    print("Ошибка: введено не число!")
+    exit(1)
+}
+
+print("Введите количество калорий в третьем продукте:")
+guard let input3 = readLine(), let calories3 = Double(input3) else {
+    print("Ошибка: введено не число!")
+    exit(1)
+}
+
+let total = calories1 + calories2 + calories3
+
+print("-------------------")
+print("Всего калорий: \(total) ккал")
 
